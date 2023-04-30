dcf1e7b785a02bc725527edb835ce50c0bb7787a
alisam163 <alisam163@yandex.ru>
2023-04-30 15:46:44 +0300

    v.2

--- src/component/Square.jsx
+++ src/component/Square.jsx
@@ -7,13 +7,3 @@ export default function Square(props) {
     </button>
   );
 }
-
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button className="square" onClick={this.props.onClick}>
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
